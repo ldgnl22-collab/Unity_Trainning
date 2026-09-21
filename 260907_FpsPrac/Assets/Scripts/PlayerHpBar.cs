34c14d27fcf66bb6033bfe02d59b258f3b63bfb6
ldgnl22-collab <ldgnl22@gmail.com>
2026-09-21 17:22:00 +0900

    [Feat] Hp 바 구현

--- 260907_FpsPrac/Assets/Scripts/PlayerHpBar.cs
+++ 260907_FpsPrac/Assets/Scripts/PlayerHpBar.cs
@@ -8,21 +8,34 @@ public class PlayerHpBar : MonoBehaviour
 {
     [SerializeField] private Image _hpGauge;
     [SerializeField] private TextMeshProUGUI _hpGaugeText;
-
-    private void Awake()
-    {
-        
-    }
+    
+    private PlayerStat _playerStat;
+    
+    private void Awake() => CacheComponents();
+    private void Start() => Init();
     
     public void SetPlayerHpBar(int currentHp, int maxHp)
     {
-        int hp = currentHp / maxHp;
-        _hpGauge.fillAmount = hp * 100;
-        
-        _hpGaugeText.text = $"{hp}/{maxHp}";
+        if (currentHp >= maxHp)
+        {
+            currentHp = maxHp;
+        }
+        else if(currentHp <= 0)
+        {
+            currentHp = 0;
+            Debug.Log($"사망");
+        }
+        _hpGauge.fillAmount = (float)currentHp / maxHp;
+        _hpGaugeText.text = $"{currentHp} / {maxHp}";
     }
 
     private void Init()
     {
+        _hpGaugeText.text = $"{_playerStat.HP} / {_playerStat.MAX_HP}";
+    }
+    
+    private void CacheComponents()
+    {
+        _playerStat = GetComponent<PlayerStat>();
     }
 }
